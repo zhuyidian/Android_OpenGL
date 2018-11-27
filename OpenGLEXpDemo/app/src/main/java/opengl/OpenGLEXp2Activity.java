43a105bc3749354ee0705584416c81abdc3ce676
ydzhu <dunnsmailface@gmail.com>
2018-11-27 18:30:21 +0800

    action:debug

--- OpenGLEXpDemo/app/src/main/java/opengl/OpenGLEXp2Activity.java
+++ OpenGLEXpDemo/app/src/main/java/opengl/OpenGLEXp2Activity.java
@@ -7,8 +7,10 @@ import android.util.DisplayMetrics;
 import android.view.Window;
 import android.view.WindowManager;
 
+import opengl.tools.CoordinateTransformation;
 import opengl.tools.DataManage;
 import opengl.view.OpenGLViewGrid;
+import opengl.view.OpenGLViewTest;
 import opengl.view.OpenGLViewTriangle;
 import opengl.view.Tree3DSurfaceView;
 
@@ -16,65 +18,78 @@ public class OpenGLEXp2Activity extends Activity {
 	private OpenGLViewGrid mOpenGLViewGrid;
     private OpenGLViewTriangle mOpenGLViewTriangle;
     private Tree3DSurfaceView mTree3DSurfaceView;
+    private OpenGLViewTest mOpenGLViewTest;
     public static float WIDTH;
     public static float HEIGHT;
 	private float w;
 	private float h;
+	private enum ChoiceOpenglDemoEnum{
+	    GRID_OPENGL,TRIANGLE_OPENGL,TREE3D_OPENGL,TEST_OPENGL
+    }
+    private ChoiceOpenglDemoEnum choiceOpenglDemoEnum = ChoiceOpenglDemoEnum.TEST_OPENGL;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //ȥ������
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //����ȫ��
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         DataManage.init(this.getResources());
 
-        //����
-//        mOpenGLView = new OpenGLViewGrid(this);
-//        DisplayMetrics md = new DisplayMetrics();
-//        getWindowManager().getDefaultDisplay().getMetrics(md);
-//        w = (float)(md.widthPixels);
-//        h = (float)(md.heightPixels);
-//        mOpenGLView.setWH(w, h);
-
-        //����
-//        mOpenGLViewTriangle = new OpenGLViewTriangle(this);
-
-        //3D tree
-        //���ϵͳ�Ŀ���Լ��߶�
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        if (dm.widthPixels > dm.heightPixels) {
-            WIDTH = dm.widthPixels;
-            HEIGHT = dm.heightPixels;
-        } else {
-            WIDTH = dm.heightPixels;
-            HEIGHT = dm.widthPixels;
+        if(choiceOpenglDemoEnum == ChoiceOpenglDemoEnum.GRID_OPENGL) {
+            mOpenGLViewGrid = new OpenGLViewGrid(this);
+            DisplayMetrics md = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(md);
+            w = (float)(md.widthPixels);
+            h = (float)(md.heightPixels);
+            mOpenGLViewGrid.setWH(w, h);
+            setContentView(mOpenGLViewGrid);
+        } else if(choiceOpenglDemoEnum == ChoiceOpenglDemoEnum.TRIANGLE_OPENGL) {
+            mOpenGLViewTriangle = new OpenGLViewTriangle(this);
+            setContentView(mOpenGLViewTriangle);
+        } else if(choiceOpenglDemoEnum == ChoiceOpenglDemoEnum.TREE3D_OPENGL) {
+            DisplayMetrics dm = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(dm);
+            if (dm.widthPixels > dm.heightPixels) {
+                WIDTH = dm.widthPixels;
+                HEIGHT = dm.heightPixels;
+            } else {
+                WIDTH = dm.heightPixels;
+                HEIGHT = dm.widthPixels;
+            }
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            mTree3DSurfaceView = new Tree3DSurfaceView(this);
+            mTree3DSurfaceView.requestFocus();
+            mTree3DSurfaceView.setFocusableInTouchMode(true);
+            setContentView(mTree3DSurfaceView);
+        } else if(choiceOpenglDemoEnum == ChoiceOpenglDemoEnum.TEST_OPENGL) {
+            DisplayMetrics md = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(md);
+            int ww = md.widthPixels;
+            int hh = md.heightPixels;
+            CoordinateTransformation.setWH(ww, hh);
+            mOpenGLViewTest = new OpenGLViewTest(this);
+            setContentView(mOpenGLViewTest);
         }
-        //����Ϊ����ģʽ
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        mTree3DSurfaceView = new Tree3DSurfaceView(this);
-        mTree3DSurfaceView.requestFocus();//��ȡ����
-        mTree3DSurfaceView.setFocusableInTouchMode(true);//����Ϊ�ɴ���
-
-        setContentView(mTree3DSurfaceView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(mTree3DSurfaceView!=null) {
-            mTree3DSurfaceView.onResume();
+        if(choiceOpenglDemoEnum == ChoiceOpenglDemoEnum.TREE3D_OPENGL) {
+            if (mTree3DSurfaceView != null) {
+                mTree3DSurfaceView.onResume();
+            }
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(mTree3DSurfaceView!=null) {
-            mTree3DSurfaceView.onPause();
+        if(choiceOpenglDemoEnum == ChoiceOpenglDemoEnum.TREE3D_OPENGL) {
+            if (mTree3DSurfaceView != null) {
+                mTree3DSurfaceView.onPause();
+            }
         }
     }
 }
